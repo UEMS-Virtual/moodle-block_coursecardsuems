924e1400aa1d5c71cd854282f8321a8472bc50d8
Breno Augusto <brenoaugusto@uems.br>
2026-05-13 13:10:12 -0400

    Renomeia bloco para Disciplinas EaD UEMS

--- lang/en/block_coursecardsuems.php
+++ lang/en/block_coursecardsuems.php
@@ -22,12 +22,12 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'UEMS course cards';
+$string['pluginname'] = 'UEMS EaD disciplines';
 $string['currentsemester'] = 'Semester {$a}';
 $string['semesterprefix'] = 'Semester';
-$string['coursecardsuems:addinstance'] = 'Add a UEMS course cards block';
-$string['coursecardsuems:myaddinstance'] = 'Add a UEMS course cards block to Dashboard';
-$string['coursecardsuems:viewcontent'] = 'View UEMS course cards block content as a student';
+$string['coursecardsuems:addinstance'] = 'Add a UEMS EaD disciplines block';
+$string['coursecardsuems:myaddinstance'] = 'Add a UEMS EaD disciplines block to Dashboard';
+$string['coursecardsuems:viewcontent'] = 'View UEMS EaD disciplines block content as a student';
 $string['coursecolormap'] = 'Course/group color map';
 $string['coursecolormap_desc'] = 'JSON with hexadecimal colors by course/group key. Example: {"PEDG24":"#ec407a","PEDG24-REO":"#f8bbd0"}. Keys such as PEDG24, PEDG-24 and PEDG_24 are normalized. For REO and REO2, use the -REO suffix. When no valid color is found, the stripe keeps the default blue.';
 $string['availablecomingsoon'] = 'Available soon';
--- lang/pt_br/block_coursecardsuems.php
+++ lang/pt_br/block_coursecardsuems.php
@@ -22,12 +22,12 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'Resumo dos cursos';
+$string['pluginname'] = 'Disciplinas EaD UEMS';
 $string['currentsemester'] = 'Semestre {$a}';
 $string['semesterprefix'] = 'Semestre';
-$string['coursecardsuems:addinstance'] = 'Adicionar bloco Cards de cursos UEMS';
-$string['coursecardsuems:myaddinstance'] = 'Adicionar bloco Cards de cursos UEMS ao Painel';
-$string['coursecardsuems:viewcontent'] = 'Ver conteúdo do bloco Cards de cursos UEMS como estudante';
+$string['coursecardsuems:addinstance'] = 'Adicionar bloco Disciplinas EaD UEMS';
+$string['coursecardsuems:myaddinstance'] = 'Adicionar bloco Disciplinas EaD UEMS ao Painel';
+$string['coursecardsuems:viewcontent'] = 'Ver conteúdo do bloco Disciplinas EaD UEMS como estudante';
 $string['coursecolormap'] = 'Mapa de cores por curso/turma';
 $string['coursecolormap_desc'] = 'JSON com cores hexadecimais por sigla de curso/turma. Exemplo: {"PEDG24":"#ec407a","PEDG24-REO":"#f8bbd0"}. Chaves como PEDG24, PEDG-24 e PEDG_24 são normalizadas. Para REO e REO2, use o sufixo -REO. Quando uma cor não for encontrada ou for inválida, a faixa mantém o azul padrão.';
 $string['availablecomingsoon'] = 'Disponível em breve';
